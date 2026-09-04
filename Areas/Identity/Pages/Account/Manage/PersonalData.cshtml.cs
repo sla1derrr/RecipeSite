00c9bb73255204d8889ade040b5d746a81ae0507
Andrew <frimang1234@gmail.com>
2026-09-04 14:09:27 +0200

    Bug fixes

--- Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace RecipeSite.Areas.Identity.Pages.Account.Manage
 {
@@ -10,11 +11,16 @@ namespace RecipeSite.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<PersonalDataModel> _logger;
 
-        public PersonalDataModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public PersonalDataModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<PersonalDataModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
 
         [BindProperty]
@@ -27,14 +33,29 @@ namespace RecipeSite.Areas.Identity.Pages.Account.Manage
             public string Password { get; set; }
         }
 
-        public IActionResult OnGet() => Page();
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Не удалось загрузить пользователя с ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
+        }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid) return Page();
-
             var user = await _userManager.GetUserAsync(User);
-            if (user == null) return NotFound();
+            if (user == null)
+            {
+                return NotFound($"Не удалось загрузить пользователя с ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
             if (!await _userManager.CheckPasswordAsync(user, Input.Password))
             {
@@ -42,8 +63,16 @@ namespace RecipeSite.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
-            await _userManager.DeleteAsync(user);
+            var result = await _userManager.DeleteAsync(user);
+            var userId = await _userManager.GetUserIdAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new System.InvalidOperationException($"Произошла неожиданная ошибка при удалении пользователя.");
+            }
+
             await _signInManager.SignOutAsync();
+            _logger.LogInformation("Пользователь с ID '{UserId}' удалил свой аккаунт.", userId);
+
             return Redirect("~/");
         }
     }
